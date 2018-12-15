af28c92b6b5c4010f2a55a8c097c3048d86b7962
Keith Needels <kneedels@gmail.com>
2018-12-15 21:02:47 +0000

    Support centering

--- ftsandbox.c
+++ ftsandbox.c
@@ -5,19 +5,27 @@
 #include FT_FREETYPE_H
 #include FT_GLYPH_H
 
+#define MAX_GLYPHS 100
+
 void draw_to_console(FT_Bitmap* bitmap, char printChar); 
 void clear_bitmap(BMP* bitmap, UCHAR r, UCHAR g, UCHAR b);
 void draw_to_bmp(FT_Bitmap* glyph_bitmap, BMP* dest_bitmap, FT_Int gbmp_left,
         FT_Int gbmp_top);
-
+void compute_string_bbox(FT_BBox *abbox, FT_Glyph* glyphs, FT_UInt num_glyphs,
+        FT_Vector* pos);
 
 int main(int argc, char *argv[]){
-    
+   
+    int output_width = 1000;
+    int output_height = 1000;
+    int dpi = 144;
+    int pointSize = 12;
+
     char printChar = argc > 2 ? argv[2][0] : '#';
     FT_Library library;
     FT_Face face;
     FT_Bool use_kerning = 1;
-    FT_UInt previous;
+    FT_UInt previous = 0;
 
     // Init FreeType library
     int error = FT_Init_FreeType(&library);
@@ -28,7 +36,8 @@ int main(int argc, char *argv[]){
     } 
 
     // Load the font file
-    error = FT_New_Face(library, "/usr/share/fonts/dejavu/DejaVuSans.ttf", 0, &face);
+    error = FT_New_Face(library, "/usr/share/fonts/dejavu/DejaVuSans.ttf", 
+            0, &face);
 
     if (error == FT_Err_Unknown_File_Format) {
         printf("Unknown font file format.\n");
@@ -42,9 +51,9 @@ int main(int argc, char *argv[]){
     error = FT_Set_Char_Size(
             face,
             0,
-            16*64, 
-            300,
-            300);
+            pointSize*64, 
+            dpi,
+            dpi);
 
     if (error){
         printf("Error setting char size: %d\n", error);
@@ -56,15 +65,19 @@ int main(int argc, char *argv[]){
     int pen_x, pen_y, n;
     FT_GlyphSlot slot = face->glyph;
 
-    pen_x = 300;
-    pen_y = 300;
+    FT_Glyph glyphs[MAX_GLYPHS];
+    FT_Vector pos[MAX_GLYPHS];
+    FT_UInt num_glyphs = 0;
+
+    pen_x = 0;
+    pen_y = 0;
 
     // Use kerning if requested and available
     use_kerning = use_kerning && FT_HAS_KERNING(face);
   
     // Create the output bitmap and clear it with white 
     BMP* bmp;
-    bmp = BMP_Create(1000, 1000, 24);
+    bmp = BMP_Create(output_width, output_height, 24);
     clear_bitmap(bmp, 255, 255, 255);
 
     for (n = 0; n < num_chars; n++){
@@ -80,12 +93,23 @@ int main(int argc, char *argv[]){
             pen_x += delta.x >> 6;
         }
 
+        pos[num_glyphs].x = pen_x;
+        pos[num_glyphs].y = pen_y;
+
         error = FT_Load_Glyph(face, glyph_index, FT_LOAD_DEFAULT);
         if (error){
             printf("Glyph load error! %d\n", error);
             continue;
         }
 
+        error = FT_Get_Glyph(face->glyph, &glyphs[num_glyphs]);
+        if (error){
+            printf("Get glyph error! %d\n", error);
+            continue;
+        }
+
+        /*
+         * MOVE THIS:
         error = FT_Render_Glyph(face->glyph, FT_RENDER_MODE_NORMAL);
         if (error){
             printf("Glyph render error! %d\n", error);
@@ -98,16 +122,87 @@ int main(int argc, char *argv[]){
                 bmp,
                 pen_x + slot->bitmap_left,
                 pen_y - slot->bitmap_top);
-
+        */
         pen_x += slot->advance.x >> 6;
         pen_y += slot->advance.y >> 6;
         previous = glyph_index;
+        num_glyphs++;
+    }
+
+    FT_BBox string_bbox;
+    compute_string_bbox(&string_bbox, glyphs, num_glyphs, pos);
+
+    FT_Pos string_width = string_bbox.xMax - string_bbox.xMin;
+    FT_Pos string_height = string_bbox.yMax - string_bbox.yMin;
+
+    FT_Pos start_x = ((output_width - string_width) / 2) * 64;
+    FT_Pos start_y = ((output_height - string_height) / 2) * 64;
+
+    for (n = 0; n < num_glyphs; n++){
+        FT_Glyph image;
+        FT_Vector pen;
+
+        image = glyphs[n];
+
+        pen.x = start_x + pos[n].x * 64;
+        pen.y = start_y + pos[n].y * 64;
+
+        error = FT_Glyph_To_Bitmap(&image, FT_RENDER_MODE_NORMAL, &pen, 0);
+
+        if (!error){
+            FT_BitmapGlyph bit = (FT_BitmapGlyph)image;
+
+            draw_to_bmp(&bit->bitmap, bmp, bit->left, output_height - bit->top);
+
+            FT_Done_Glyph(image);
+
+        }
     }
 
     BMP_WriteFile(bmp, "out.bmp");
     BMP_Free(bmp);
     printf("Success!\n");
     return 0;
+}
+
+void compute_string_bbox(FT_BBox *abbox, FT_Glyph* glyphs, FT_UInt num_glyphs,
+        FT_Vector* pos){
+    FT_BBox bbox;
+    FT_BBox glyph_bbox;
+    int n;
+
+    bbox.xMin = bbox.yMin = 32000;
+    bbox.xMax = bbox.yMax = -32000;
+
+    for (n = 0; n < num_glyphs; n++){
+        FT_Glyph_Get_CBox(glyphs[n], ft_glyph_bbox_pixels, &glyph_bbox);
+
+        glyph_bbox.xMin += pos[n].x;
+        glyph_bbox.xMax += pos[n].x;
+        glyph_bbox.yMin += pos[n].y;
+        glyph_bbox.yMax += pos[n].y;
+
+        if (glyph_bbox.xMin < bbox.xMin)
+            bbox.xMin = glyph_bbox.xMin;
+
+        if (glyph_bbox.yMin < bbox.yMin)
+            bbox.yMin = glyph_bbox.yMin;
+        
+        if (glyph_bbox.xMax > bbox.xMax)
+            bbox.xMax = glyph_bbox.xMax;
+
+        if (glyph_bbox.yMax > bbox.yMax)
+            bbox.yMax = glyph_bbox.yMax;
+    }
+
+    if (bbox.xMin > bbox.xMax){
+        bbox.xMin = 0;
+        bbox.yMin = 0;
+        bbox.xMax = 0;
+        bbox.yMax = 0;
+    }
+
+    *abbox = bbox;
 }
 
 void clear_bitmap(BMP* bitmap, UCHAR r, UCHAR g, UCHAR b) {
